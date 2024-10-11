173491b9a45100c8f7d15c852a56655ddad81a27
shabeelashraf5 <05shebz@gmail.com>
2024-10-11 17:13:58 +0530

    Modified config from server side

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,9 @@
 const mongoose = require('mongoose')
 
-// mongoose.set('strictQuery', false)
-// mongoose.connect('mongodb+srv://shabeelash5:wGzEMe0qZvBVAgN6@cluster0.kfyrp1e.mongodb.net/pass-gen?retryWrites=true&w=majority')
-// .then(() => {
-//     console.log('The Database Connected')
-// })
-// .catch((error) => {
-//     console.log('Failed to Connect', error)
-
-// })
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect('mongodb+srv://shabeelash5:wGzEMe0qZvBVAgN6@cluster0.kfyrp1e.mongodb.net/pass-gen?retryWrites=true&w=majority', {
+        await mongoose.connect(process.env.MONGO_URI, {
         });
         console.log('Database connected successfully');
     } catch (error) {
