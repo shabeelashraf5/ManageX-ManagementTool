f5837b61d44774cb035e4361c567272ca85d22a5
shabeelashraf5 <05shebz@gmail.com>
2025-01-08 18:56:59 +0530

    Env modification

--- client/src/environment/environment.ts
+++ client/src/environment/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
 
     production : false,
     //apiUrl: 'http://localhost:3000'
-    apiUrl: 'https://managex-managementtool.onrender.com'
+    // apiUrl: 'https://managex-managementtool.onrender.com'
     
 }
